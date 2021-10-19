8f2aa71d1d8762ca5baa53a445d4f3eb1e0ee76f
Sonrak141 <cborrero094@gmail.com>
2021-10-18 19:39:02 -0500

    mad

---
+++ constants/db.tsx
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDd_YuLqPY0-SFEgvl07Nw4BmTZw9H0csY",
+  authDomain: "organizen-81cdc.firebaseapp.com",
+  databaseURL: "https://organizen-81cdc-default-rtdb.firebaseio.com",
+  projectId: "organizen-81cdc",
+  storageBucket: "organizen-81cdc.appspot.com",
+  messagingSenderId: "197746246481",
+  appId: "1:197746246481:web:805a11f143cde7cb384970"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
